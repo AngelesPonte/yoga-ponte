cc346a3ed3d7db18246fbc191a3607f7823ad571
Angeles Ponte <ponteangeles@gmail.com>
2021-04-17 09:59:53 -0300

    cleanning app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Navbar, Nav, Form, FormControl } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="">LiveYogi</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Full Library</Nav.Link>
+      <Nav.Link href="#method">Method</Nav.Link>
+      <Nav.Link href="#practice">My Practice</Nav.Link>
+    </Nav>
+      <Button variant="outline-info">Sign Up</Button>
+      <Button variant="outline-info">Log In</Button>
+  </Navbar>
+  
     </div>
   );
 }
