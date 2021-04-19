a9bfdc62bf886816b55e6ddece68f5555c8ca718
Angeles Ponte <ponteangeles@gmail.com>
2021-04-19 14:32:34 -0300

    desafio2

--- src/Components/NavBar/navbar.js
+++ src/Components/NavBar/navbar.js
@@ -1,26 +1,46 @@
 import { Button, Navbar, Nav, Form, FormControl } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './navbar.css';
-
+import logo from '../../../src/img/lylogo.png';
 export const Navbare = (props) => {
       return(
-        <header>
-            <img src="" alt="logo" />
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="">LiveYogi</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Full Library</Nav.Link>
-                    <Nav.Link href="#method">Method</Nav.Link>
-                    <Nav.Link href="#practice">My Practice</Nav.Link>
-                </Nav>
-                <div>
-                    <span>
-                    <img src="" alt="Cart" />
-                    <p>{props.cartQty}</p>
-                    </span>
-                    <img src={props.avatar} alt="Cart" />
-                </div>
-            </Navbar>
+        <header >
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <img src={logo} className="logo" alt="Live Yogi" />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+            <Nav.Link href="#library">Full Library</Nav.Link>
+            <Nav.Link href="#method">My Method</Nav.Link>
+            <Nav.Link href="#practice">My Practice</Nav.Link>
+            </Nav>
+            <Nav>
+            <Nav.Link href="#deets">Sign Up</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+                Sign In
+            </Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+            
         </header>
     )
 }
+
+//<img src="" alt="logo" />
+//<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+//<Navbar.Brand href="">LiveYogi</Navbar.Brand>
+//<Nav className="mr-auto">
+//    <Nav.Link href="#home">Full Library</Nav.Link>
+ //   <Nav.Link href="#method">Method</Nav.Link>
+//    <Nav.Link href="#practice">My Practice</Nav.Link>
+//</Nav>
+//<div>
+ //   <span>
+ //   <img src="" alt="Cart" />
+ //   <p>{props.cartQty}</p>
+ //   </span>
+//    <img src={props.avatar} alt="Cart" />
+//</div>
+//</Navbar>
+//
